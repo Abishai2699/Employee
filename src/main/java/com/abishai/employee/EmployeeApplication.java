dd00dafa753e9ccf6343696ad91dc822d0b70e63
Abishai2699 <78915155+Abishai2699@users.noreply.github.com>
2022-01-14 13:27:37 +0530

    Update EmployeeApplication.java

--- src/main/java/com/abishai/employee/EmployeeApplication.java
+++ src/main/java/com/abishai/employee/EmployeeApplication.java
@@ -23,8 +23,8 @@ public class EmployeeApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 	
-		Employee e1 = new Employee("Vikram","1987/08/09","2020/06/27",90000.00,"vikram@abc.com",12345);   //passing values for the object e1
-		Employee e2 = new Employee("Kamal","1995/04/08","2020/09/29",60000.00,"Kamal@abc.com",123456);  //passing values for the object e2
+		Employee e1 = new Employee("Vikram", "1987/08/09", "2020/06/27", 90000.00, "vikram@abc.com", 567);   //passing values for the object e1
+		Employee e2 = new Employee("Kamal", "1995/04/08", "2020/09/29", 60000.00, "Kamal@abc.com", 789);  //passing values for the object e2
 		
 		rep.employee.addAll(Arrays.asList(e1,e2));  // adds employee details in employee list
 		
